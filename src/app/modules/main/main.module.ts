b8d42d45cd7c696c6b8a035de1917353472275d9
Ryzzan Abbade Salman <rabbadesalman@gmail.com>
2021-07-28 13:44:45 -0300

    Dashboard updated presenting the project

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -7,12 +7,14 @@ import { SharedModule } from '../shared/shared.module';
 /** Components */
 import { MainComponent } from './main.component';
 import { ProfileComponent } from 'src/app/components/profile/profile.component';
+import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
     ProfileComponent,
+    DashboardComponent,
   ],
   imports: [
     CommonModule,
